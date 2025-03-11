dd817f5a49a855ed73a10151adb2b25fa7dc06d4
Mohammad Kobeissi <138688228+MK0B3@users.noreply.github.com>
2025-03-11 15:12:53 +0200

    Fixed minor issues in comments page and homepage
    added thumbs down and thumbs up
    added reply button
    fixed dropdown menu

--- Home_Page/home.js
+++ Home_Page/home.js
@@ -1,27 +1,27 @@
-document.getElementById('comment-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting to a server
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdownButton = document.querySelector('.dropdown-btn');
+    var dropdownContent = document.querySelector('.dropdown-content');
 
-    var commentInput = document.getElementById('comment-input');
-    if (commentInput.value.trim() !== "") {
-        var newComment = document.createElement('li');
-        newComment.className = 'comment-main-level';
-        newComment.innerHTML = `
-            <div class="comment-avatar"><img src="http://i9.photobucket.com/albums/a88/creaticode/avatar_4_zps6f333073.jpg" alt=""></div>
-            <div class="comment-box">
-                <div class="comment-head">
-                    <h6 class="comment-name"><a href="#">New Commenter</a></h6>
-                    <span>just now</span>
-                    <i class="fa fa-reply"></i>
-                    <i class="fa fa-heart"></i>
-                </div>
-                <div class="comment-content">
-                    ${commentInput.value}
-                </div>
-            </div>
-        `;
-        document.getElementById('comments-list').appendChild(newComment);
-        commentInput.value = ''; // Clear the input after adding
-    } else {
-        alert("Please enter a comment.");
-    }
+    // Toggle the dropdown content on click
+    dropdownButton.addEventListener('click', function() {
+        // Check if dropdown is already displayed, toggle accordingly
+        if (dropdownContent.style.display === 'block') {
+            dropdownContent.style.display = 'none';
+        } else {
+            dropdownContent.style.display = 'block';
+        }
+    });
+
+    // Close the dropdown when clicking outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropdown-btn')) {
+            var dropdowns = document.getElementsByClassName('dropdown-content');
+            for (var i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.style.display === 'block') {
+                    openDropdown.style.display = 'none';
+                }
+            }
+        }
+    };
 });
